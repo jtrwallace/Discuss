4eb5143e54fafa8f745e8de36e73a8298b05b112
Bryan Truong <btruong1@ucsc.edu>
2015-11-09 19:17:27 -0800

    Created tables.py
    added user database

---
+++ models/tables.py
@@ -0,0 +1,7 @@
+db.define_table('user',
+                Field('name'),
+                Field('city'),
+                Field('email'),
+                Field,('password'),
+                Field('age'),
+                Field('gender'))
